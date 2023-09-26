a80739766adfb9c497262c391f1b9f7dbe20f727
k3lly003 <ntirukelly@gmail.com>
2023-09-26 17:15:02 +0300

    initial commit

---
+++ git.readme.js
@@ -0,0 +1,12 @@
+setInterval(draw, 50);
+var typed = new Typed(".auto-input", {
+  strings: [
+    "Hey!",
+    "I'm Ntirushwa.",
+    "Brice Kelly",
+    "A MEARN Stack Developer.",
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+});
